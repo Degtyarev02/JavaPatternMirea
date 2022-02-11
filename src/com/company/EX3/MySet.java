f39f013fa8551be5637ee6a3422f85bbc9321a43
Vladimir <vladimirdegty@gmail.com>
2022-02-11 17:40:11 +0300

    main

---
+++ src/com/company/EX3/MySet.java
@@ -0,0 +1,104 @@
+package com.company.EX3;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class MySet<T> implements Set<T> {
+
+    Set<T> set;
+
+    public MySet() {
+        set = new HashSet<>();
+    }
+
+
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
+
+    @Override
+    public int size() {
+        return set.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return set.contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        try{
+            writeLock.lock();
+            return set.add(t);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        try{
+            writeLock.lock();
+            return set.remove(o);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String toString() {
+        return set.toString();
+    }
+}
---
+++ src/com/company/EX3/Test.java
@@ -0,0 +1,37 @@
+package com.company.EX3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+
+    public static void main(String[] args) {
+        MySet<Integer> syncSet = new MySet<>();/*
+        Set<Integer> syncSet = new HashSet<>();*/
+
+
+        Thread th1 = new Thread(() -> {
+           for(int i = 0; i < 1000; i++) {
+               syncSet.add(i);
+           }
+        });
+
+        Thread th2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                syncSet.remove(i);
+            }
+        });
+
+        th1.start();
+        th2.start();
+
+        try{
+            th1.join();
+            th2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(syncSet);
+    }
+}
